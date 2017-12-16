edaaf0e6fe251c2d2a20b1439628e0dd67bc2014
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-16 17:56:08 -0600

    beta:change beta.rb

--- beta/beta.rb
+++ beta/beta.rb
@@ -1,11 +1,30 @@
-def multiply(x, y)
-    return x * y
+def choice(num)
+    case num.to_i
+when 1
+    print "Ger mig en nombra: "
+    num1 = gets.chomp.to_i
+    print "Ger mig en nombra: "
+    num2 = gets.chomp.to_i
+    puts multiply(num1,num2)
+when 2
+    print "Ger mig en nombra: "
+    num1 = gets.chomp.to_i
+    print "Ger mig en nombra: "
+    num2 = gets.chomp.to_i
+    puts divide(num1,num2)
+end
 end
 
-print "Ger mig en nombra: "
-num1 = gets.chomp.to_i
-puts num1
-print "Ger mig en nombra: "
-num2 = gets.chomp.to_i
-puts num2
-puts "#{num1} \* #{num2} = #{multiply(num1,num2)}"
+def multiply(x, y)
+    return "#{x} * #{y} = #{x * y}"
+end
+
+def divide(x,y)
+    return "#{x} / #{y} = #{x / y}"
+end
+
+puts "What kind of calculation?"
+puts "1. Multiply"
+puts "2. Divide"
+choice = gets.chomp
+choice(choice)
