55125bb6ad20db4e450c015e826d5f0b6aa8f7e3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-16 18:23:51 -0600

    gamma:test different iterators

--- gamma/gamma.rb
+++ gamma/gamma.rb
@@ -1,9 +1,19 @@
 def randomArr
     a = []
-    rand(100).times do
-        a.push(rand(100))
-    end
-    return a
+    100.times { a.push(rand(100)) }
+    return a.uniq!
 end
 
-puts randomArr
+puts "#{randomArr.length} #{randomArr}"
+
+def forLoop
+    a = []
+    b = []
+    100.times do
+        a.push(rand(100))
+    end
+    b.push(a.select { |num| num == 10 })
+    return b
+end
+
+puts forLoop
