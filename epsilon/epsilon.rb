709b15b0425e53be5e08c7c0292ae687c937f6d3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 19:31:24 -0600

    make formCity method

---
+++ epsilon/epsilon.rb
@@ -0,0 +1,12 @@
+
+def formCity
+    possibles = ['a','e','i','o','u','st','sk','ba','bo','bi','ch','ca','co','da','de','fe','fa','ma','mo','na','th']
+    possiblesArr = []
+    rand(10).times { possiblesArr.push(possibles[rand(possibles.length)]) }
+    formCities(possiblesArr)
+end
+
+def formCities(city)
+    
+    rand(10).times { }
+end
