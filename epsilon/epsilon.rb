ebf590c80030f976577212fce753b48ca4390d7c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 19:34:55 -0600

    make make_city method and refactor code to fit Ruby Style conventions

--- epsilon/epsilon.rb
+++ epsilon/epsilon.rb
@@ -1,12 +1,14 @@
 
-def formCity
+def form_city
     possibles = ['a','e','i','o','u','st','sk','ba','bo','bi','ch','ca','co','da','de','fe','fa','ma','mo','na','th']
     possiblesArr = []
     rand(10).times { possiblesArr.push(possibles[rand(possibles.length)]) }
-    formCities(possiblesArr)
+    makeCities(possiblesArr)
 end
 
-def formCities(city)
-    
-    rand(10).times { }
+def make_city(arr)
+    form_area_codes(arr.join(''))
+end
+
+def form_area_codes(city)
 end
