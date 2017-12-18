a0b42df7975409a3cca440c903ee8c9c7afe2a6b
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 19:57:37 -0600

    epsilon: generates a directory of randomly generate city names with corresponding area codes

--- epsilon/epsilon.rb
+++ epsilon/epsilon.rb
@@ -1,6 +1,6 @@
 def form_area_codes(city)
     area_code = [rand(9), rand(9), rand(9)]
-    return {city: city, code: area_code}
+    return {city: city, code: area_code.join('')}
 end
 
 def form_city
@@ -10,10 +10,27 @@ def form_city
     form_area_codes(possiblesArr.join(''))
 end
 
+
 def make_directory
     directory = []
     rand(10).times { directory.push(form_city) }
+    directory.each { |k,v| directory.delete(k) if k === "" }
     return directory
 end
 
-puts make_directory
+def show_dir
+    dir = make_directory
+    puts "The cities available are: "
+    dir.each { |k, v| puts "#{k[:city]} at AC: #{k[:code]}" }
+end
+
+
+loop do
+    puts "Would you like to look up a city?"
+    puts "(Y/N)"
+    input = gets.chomp
+    if input.upcase === "Y"
+        show_dir
+        break
+    end
+end
