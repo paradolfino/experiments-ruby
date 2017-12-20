aaf3a89970ca5343264f275ca00367dfb53ff7e3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-19 20:16:36 -0600

    zeta: oop practice

--- zeta/user.rb
+++ zeta/user.rb
@@ -1,9 +1,23 @@
 class User
-
+    def initialize(name)
+        @name = name
+    end
+    def run
+        puts "runs"
+    end
+    def get_name
+        @name
+    end
+    def set_name(name)
+        @name = name
+    end
 end
 
-user = User.new
+user = User.new("Gotha")
+user1 = User.new("Frast")
+user2 = User.new("Snard")
 
-puts User.ancestors
-
-puts user
+user.run
+puts user.get_name
+user.set_name("Lol")
+puts user.get_name
