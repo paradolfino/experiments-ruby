dfa6558cb62c703ddee93b7ff7d152e53606d706
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-20 21:07:33 -0600

    inheritance practice

--- zeta/user.rb
+++ zeta/user.rb
@@ -10,6 +10,18 @@ class User
     
 end
 
+class Buyer < User
+    def run
+        
+    end
+end
+
+class Seller < User
+end
+
+class Admin < User
+end
+
 user = User.new('','')
 
 puts "What is your name?"
@@ -18,3 +30,6 @@ puts "What is your email?"
 user.email = gets.chomp
 puts "Ah, so you are #{user.name} with an address of #{user.email}"
 
+buyer1 = Buyer.new("Johan Gor","johangor@zort.com")
+buyer1.run
+seller1 = Seller.new("Frank Frast","morgulstagger@frost.com")
