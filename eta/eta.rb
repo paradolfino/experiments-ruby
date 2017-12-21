7ad7d00dd2872a6072a346fc39ddb1fcd2fcfb72
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-20 21:26:41 -0600

    eta: add eta class with init and methods

--- eta/eta.rb
+++ eta/eta.rb
@@ -3,3 +3,18 @@
 #Creates a new user with the assigned permissions
 #Saves the user in a new users listing file, if file doesn't exist yet it creates one
 
+require 'json'
+
+class Eta
+    attr_accessor :email, :name, :permissions
+    def initialize(*args)
+        @email = args[0]
+        @name = args[1]
+        @permissions = Eta.permissions_from_template
+    end
+
+    def self.permissions_from_template
+        file = File.read 'eta_permissions_template.json'
+        JSON.load(file, nil, symbolize_names: true)
+    end
+end
