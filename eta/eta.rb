8ca6a428944bf9913f350ba9ab86bc1f6f8d7bc0
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-20 21:29:36 -0600

    eta: change eta_users.json to eta.json and add save method

--- eta/eta.rb
+++ eta/eta.rb
@@ -17,4 +17,11 @@ class Eta
         file = File.read 'eta_permissions_template.json'
         JSON.load(file, nil, symbolize_names: true)
     end
+
+    def save
+        self_json = {email: @email, name: @name, permissions: @permissions}.to_json
+        open('eta.json') do |file|
+            file.puts self_json
+        end
+    end
 end
