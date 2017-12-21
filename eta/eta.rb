16b9cae6d5da19801b590964e8478a77357c2fbd
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-20 21:16:43 -0600

    eta:fc

---
+++ eta/eta.rb
@@ -0,0 +1,4 @@
+#Reads in user information that is provided
+#Reads in permissions from a json file
+#Creates a new user with the assigned permissions
+#Saves the user in a new users listing file, if file doesn't exist yet it creates one
