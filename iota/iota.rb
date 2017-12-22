dc2c4b4a19ca5028f2365d2156e245c7205cde4e
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-22 15:34:13 -0600

    iota: add detect_file and commit_loop methods

--- iota/iota.rb
+++ iota/iota.rb
@@ -1,8 +1,12 @@
 
 
-def detect_file
+def detect_file(ref)
    mod = Dir.glob("*").max_by {|f| File.mtime(f)}
+   commit_loop(mod)
 end
 
-def commit_loop
+def commit_loop(times, ref)
+    sleep 1
+    times.times { detect_file }
 end
+
