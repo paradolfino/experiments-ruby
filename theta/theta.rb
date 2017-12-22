068eec715798430c60f7a2bd6733e382888e5f9f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-22 00:32:25 -0600

    'test'

--- theta/theta.rb
+++ theta/theta.rb
@@ -1,7 +1,18 @@
-system("git status")
-sleep 2
-system("git add .")
-sleep 2
-system('git commit -m "test"')
-sleep 2
-system("git push -u origin december")
+def commit_loop
+    key = ["a","b","c","d","e","f","g"]
+    token = []
+    rand(key.length).times { token.push(key[rand(key.length)]) }
+    open('commits.txt', 'a') do |file|
+        file.puts "modification: #{token}"
+    end
+    system "git add ."
+    sleep 1
+    system "git commit -m 'test'"
+end
+
+def commit_times(times)
+    sleep 1
+    times.times { commit_loop }
+end
+
+commit_times 10
