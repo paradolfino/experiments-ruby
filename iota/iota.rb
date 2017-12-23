2c987c11a48d1b9f8575909aef4446264b85b27f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-23 00:46:07 -0600

    iota: change

--- iota/iota.rb
+++ iota/iota.rb
@@ -3,15 +3,15 @@
 def detect_file
    mod = Dir.glob("*/*").max_by {|f| File.mtime(f)}
    mod = mod.split('/')
-   commit_loop(mod.pop)
+   commit_loop(mod[0], mod[mod.length])
 end
 
-def commit_loop(ref)
-    verb = ["modify","change","add to","edit"]
+def commit_loop(path, ref)
+    verb = ["modify","change","edit"]
     sleep 1
     system "git add ."
     sleep 1
-    system "git commit -m \"#{verb[rand(verb.length)]} #{ref}\""
+    system "git commit -m \"#{path}: #{verb[rand(verb.length)]} #{ref}\""
 end
 
 reaper = Thread.new do
