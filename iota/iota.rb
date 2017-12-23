3551d7cd2f7a751a2532914c5ffeb33a9247bfb8
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-23 00:47:42 -0600

    iota: modify

---
+++ iota/iota.rb
@@ -0,0 +1,28 @@
+
+
+def detect_file
+   mod = Dir.glob("*/*").max_by {|f| File.mtime(f)}
+   mod = mod.split('/')
+   commit_loop(mod)
+end
+
+def commit_loop(ref)
+    verb = ["modify","change","edit"]
+    sleep 1
+    system "git add ."
+    sleep 1
+    system "git commit -m \"#{ref[0]}: #{verb[rand(verb.length)]} #{ref[1]}\""
+end
+
+reaper = Thread.new do
+    while true
+      detect_file
+    end
+ end
+ 
+ gets
+ system "git push -u origin december"
+ reaper.kill
+#Dir.glob("*").max_by {|f| File.mtime(f)}
+#Dir.glob("#{Dir.pwd}**/").max_by {|f| File.mtime(f)}
+#Dir.glob("*/*").max_by {|f| File.mtime(f)}
