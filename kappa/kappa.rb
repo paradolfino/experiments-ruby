0f2d78d06d0f69b7866b48bf3b291beb261875cc
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-25 20:56:53 -0600

    commit to pool[102w04] at 20:56

--- kappa/kappa.rb
+++ kappa/kappa.rb
@@ -1,10 +1,10 @@
 #procs practice
 
-full_name = Proc.new { |first, last| first + " " + last }
+full_name = Proc.new { |first, last| first + " " + last } #single line syntax
 p full_name["vik","vol"] #bracket
 p full_name.call('val','vak') #call
 
-nam2 = Proc.new do |x,y|
+nam2 = Proc.new do |x,y| #multi line syntax
     x * y
 end
 
