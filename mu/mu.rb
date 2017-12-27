1905f9a70ece1aa6ee35abe73b9114f3d1a779f6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-26 20:58:51 -0600

    commit 5 to pool[brilliant-rolling-mountain-2j8340] at 20:58 - 26/12/2017

--- mu/mu.rb
+++ mu/mu.rb
@@ -1,9 +1,12 @@
 #new method syntax for ruby 2.3 practice
 
-def print_address city:, state:, zip:
-    puts city
-    puts state
-    puts zip
-end
+# def print_address city:, state:, zip:
+#     puts city
+#     puts state
+#     puts zip
+# end
 
-print_address city: 'Arlington', state: "Tx", zip: '76014'
+#print_address city: 'Arlington', state: "Tx", zip: '76014'
+
+def sms_generator api_key, num, msg, locale
+    
