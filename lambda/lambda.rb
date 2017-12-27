429ba8b421d2104d991721e08c0d138d8d4f505d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-26 20:50:05 -0600

    commit 3 to pool[rapid-shining-rabbit-nzxknb] at 20:50 - 26/12/2017

--- lambda/lambda.rb
+++ lambda/lambda.rb
@@ -8,3 +8,11 @@ full_name = -> (first, last) { first + " " + last }
 p full_name.call("Test","Tests")
 
 #lamdas will throw an error for wrong number of arguments
+def my_method
+    x = lambda { return }
+    x.call
+    p "Text"
+end
+
+my_method
+#lambdas will not skip the rest of the method when return is called inside lambda {}
