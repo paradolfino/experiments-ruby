63756272de9870c54b59680c083fafe0fcb8ce02
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-27 20:55:01 -0600

    commit 10 to pool[rapid-rolling-rabbit-3h65u8] at 20:55 - 27/12/2017

--- nu/nu.rb
+++ nu/nu.rb
@@ -1,5 +1,6 @@
 #Splat and Keyword Splat Arguments
 
+#Splat
 # def roster *players
 #     puts players #outputs one at a time
 #     p players #outputs array itself
@@ -7,6 +8,7 @@
 
 # roster 'Aloooo', 'Rodkin', 'Splat'
 
+#Keyword Value Splat Args
 def roster **players_with_positions
     players_with_positions.each { |k,v| puts "Player #{k}, Position #{v}" }
 end
