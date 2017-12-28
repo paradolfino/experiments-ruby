a2deccf8dc15a0f933ec811e6aaafefbdc2a4f9a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-27 20:50:36 -0600

    commit 4 to pool[rapid-rolling-rabbit-3h65u8] at 20:50 - 27/12/2017

--- nu/nu.rb
+++ nu/nu.rb
@@ -1,7 +1,8 @@
 #Splat and Keyword Splat Arguments
 
 def roster *players
-    puts players
+    puts players #outputs one at a time
+    p players #outputs array itself
 end
 
 roster 'Aloooo', 'Rodkin', 'Splat'
