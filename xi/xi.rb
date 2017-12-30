3aecd52ed0778034e3795d415fbe6e2a7ec0b034
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-29 21:16:20 -0600

    commit 9 to pool[rapid-living-river-w3n6i6] at 21:16 - 29/12/2017

--- xi/xi.rb
+++ xi/xi.rb
@@ -1,29 +1,29 @@
 #loops other than while/each
 
 #for in
-for i in 0..42
-    p i
-end
+# for i in 0..42
+#     p i
+# end
 
 #nested iterator
-obj = {
-    sub_obj1: {
-        sso1: 1,
-        sso2: 2,
-        sso3: 3,
-        sso4: 4
-    },
-    sub_obj2: {
-        sso2_1: 5,
-        sso2_2: 6,
-        sso2_3: 7,
-        sso2_4: 8
-    }
-}
+# obj = {
+#     sub_obj1: {
+#         sso1: 1,
+#         sso2: 2,
+#         sso3: 3,
+#         sso4: 4
+#     },
+#     sub_obj2: {
+#         sso2_1: 5,
+#         sso2_2: 6,
+#         sso2_3: 7,
+#         sso2_4: 8
+#     }
+# }
 
-obj.each do |key, value|
-    puts key
-    value.each do |subkey, subvalue|
-        p "#{subkey} for #{subvalue}"
-    end
-end
+# obj.each do |key, value|
+#     puts key
+#     value.each do |subkey, subvalue|
+#         p "#{subkey} for #{subvalue}"
+#     end
+# end
