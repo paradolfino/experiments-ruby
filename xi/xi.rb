b6d44ab239502b593fcecd89ecc027e11f1e7944
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-29 21:12:46 -0600

    commit 7 to pool[rapid-living-river-w3n6i6] at 21:12 - 29/12/2017

--- xi/xi.rb
+++ xi/xi.rb
@@ -6,4 +6,17 @@ for i in 0..42
 end
 
 #nested iterator
-
+obj = {
+    sub_obj1: {
+        sso1: 1,
+        sso2: 2,
+        sso3: 3,
+        sso4: 4
+    },
+    sub_obj2: {
+        sso2_1: 5,
+        sso2_2: 6,
+        sso2_3: 7,
+        sso2_4: 8
+    }
+}
