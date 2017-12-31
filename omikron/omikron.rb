b4600458b64c1aa491f1b4a73c43b8a42436febd
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-30 19:05:53 -0600

    commit 10 to pool[brilliant-living-rabbit-510e51] at 19:05 - 30/12/2017

--- omikron/omikron.rb
+++ omikron/omikron.rb
@@ -1,4 +1,4 @@
 #(0..50).to_a.map { |x| puts x.to_i }
 #(0..50).to_a.map(&:to_i)
 #("a".."g").to_a.map { |x| puts x * 2}
-Hash[[1,2.1,3.33,0.9].map { |x| [x,x.to_i] }]
+Hash[[1,2.1,3.33,0.9].map { |x| puts [x,x.to_i] }]
