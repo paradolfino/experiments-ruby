f7161173aeb980d21211a8d571ba6391c141c7c0
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-31 18:19:44 -0600

    commit 2 to pool[serene-steaming-cow-ib2w8d] at 18:19 - 31/12/2017

--- rho/rho.rb
+++ rho/rho.rb
@@ -6,7 +6,3 @@ class ApiConnector
     end
 end
 
-api = ApiConnector.new
-api.title = "Udemy"
-api.url = "https://www.udemy.com/comprehensive-ruby-programming-tutorial/learn/v4/t/lecture/4424046?start=0"
-puts api.title
