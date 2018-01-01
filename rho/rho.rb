6ce4aca676aa76881fdf39883bd9983f615f2f57
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-31 18:17:29 -0600

    commit 8 to pool[pretty-rolling-mountain-hv8arv] at 18:17 - 31/12/2017

--- rho/rho.rb
+++ rho/rho.rb
@@ -5,4 +5,4 @@ end
 api = ApiConnector.new
 api.title = "Udemy"
 api.url = "https://www.udemy.com/comprehensive-ruby-programming-tutorial/learn/v4/t/lecture/4424046?start=0"
-puts api,title
+puts api.title
