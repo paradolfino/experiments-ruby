20b777839b602b7a872933bc74bd9d22dd761288
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-02 19:26:53 -0600

    commit 6 to pool[rapid-steaming-river-3v4gcr] at 19:26 - 02/01/2018

--- sigma/sigma.rb
+++ sigma/sigma.rb
@@ -3,6 +3,8 @@ File.open("test.rb","a") do |file|
     file.puts "puts 'test'"
 end
 
+#File.open("test.rb","a") { |file| file.write("test") }
+
 # .open second param
 # r - reading
 # a - appending
