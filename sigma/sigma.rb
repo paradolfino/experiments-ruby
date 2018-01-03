129998f6d9ce03d618c91f7e312bb610d8af26d3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-02 19:37:43 -0600

    commit 25 to pool[rapid-steaming-river-3v4gcr] at 19:37 - 02/01/2018

--- sigma/sigma.rb
+++ sigma/sigma.rb
@@ -1,7 +1,7 @@
 #files - saving/creating
-File.open("test.rb","a") do |file|
-    file.puts "puts 'test'"
-end
+# File.open("test.rb","a") do |file|
+#     file.puts "puts 'test'"
+# end
 
 #File.open("test.rb","a") { |file| file.write("test") }
 #file_to_save = File.new("test.rb","a")
