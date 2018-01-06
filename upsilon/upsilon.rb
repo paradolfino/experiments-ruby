9228549a9818d5058219fe50851681b40a02f3e3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-05 20:09:35 -0600

    commit 5 to pool[serene-steaming-cow-hi8d68] at 20:09 - 05/01/2018

--- upsilon/upsilon.rb
+++ upsilon/upsilon.rb
@@ -13,7 +13,7 @@
 VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
 def is_valid_email? email
-    email =~ VALID_EMAIL_REGIX
+    email =~ VALID_EMAIL_REGEX
 end
 
 p is_valid_email? "zozo@zoho.com"
