fd308cbced44f3b8db6a9834c85ea804dea77a4f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-05 20:06:52 -0600

    commit 1 to pool[serene-steaming-cow-hi8d68] at 20:06 - 05/01/2018

--- upsilon/upsilon.rb
+++ upsilon/upsilon.rb
@@ -1,11 +1,13 @@
 #regular expressions
 
-string = "The quick 12 brown foxes jumped over the 10 lazy dogs"
+# string = "The quick 12 brown foxes jumped over the 10 lazy dogs"
 
-p string =~ /quick/
+# p string =~ /quick/
 
-p string =~ /z/ ? "Valid" : "Invalid"
+# p string =~ /z/ ? "Valid" : "Invalid"
 
-p string =~ /Z/i ? "Valid" : "Invalid" #case insensitive
+# p string =~ /Z/i ? "Valid" : "Invalid" #case insensitive
 
-p string.to_enum(:scan, /\d+/).map { Regexp.last_match } #scans and returns all of the integers
+# p string.to_enum(:scan, /\d+/).map { Regexp.last_match } #scans and returns all of the integers
+
+VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
