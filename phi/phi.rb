5100466c20eb98f1154e292642499b36a5d18b57
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-06 19:40:52 -0600

    commit 7 to pool[serene-rolling-cow-62465f] at 19:40 - 06/01/2018

--- phi/phi.rb
+++ phi/phi.rb
@@ -3,6 +3,6 @@ sent_arr = start_sent.downcase.split('')
 acc_count = start_sent - [" "] #subtract all empty spaces in array
 fin_sent = start_sent.gsub(/[a-zA-Z]/, "_").split("")
 
-while sent_arr.count("") < acc_count.length
+while sent_arr.count("") < acc_count.count
 
 end
