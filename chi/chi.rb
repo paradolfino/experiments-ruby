1d2be595ebdbdbe8c9d6c573640c32c2daeb936a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-07 14:10:12 -0600

    commit 6 to pool[rapid-steaming-river-i1quw6] at 14:10 - 07/01/2018

--- chi/chi.rb
+++ chi/chi.rb
@@ -1,4 +1,6 @@
 #grep
 arr = ['hey.rb', 'there.rb', 'index.html']
 
-p arr.select{ |x| x =~ /\.rb/ }.map{ |x| x[0..-4]} #select method
+#p arr.select{ |x| x =~ /\.rb/ }.map{ |x| x[0..-4]} #select method
+
+
