51123fd087df82e9944bb8e75adf945bbb437e1f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-08 15:01:14 -0600

    commit 6 to pool[brilliant-steaming-rabbit-qf6kfa] at 15:01 - 08/01/2018

--- psi/psi.rb
+++ psi/psi.rb
@@ -1,3 +1,12 @@
 #dish gem example
 require 'rubygems'
 require 'dish'
+
+hash = {
+    movie: "Star Wars",
+    actors: [
+        {age: 22, name: "luke"},
+        {age: 22, name: "luke"},
+        {age: 22, name: "luke"}
+    ]
+}
