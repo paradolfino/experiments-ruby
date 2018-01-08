c14460a1cd6230d9e92419b598a4d421cfbe9290
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-08 15:01:28 -0600

    commit 7 to pool[brilliant-steaming-rabbit-qf6kfa] at 15:01 - 08/01/2018

--- psi/psi.rb
+++ psi/psi.rb
@@ -6,7 +6,7 @@ hash = {
     movie: "Star Wars",
     actors: [
         {age: 22, name: "luke"},
-        {age: 22, name: "luke"},
-        {age: 22, name: "luke"}
+        {age: 35, name: "han"},
+        {age: 400, name: "yoda"}
     ]
 }
