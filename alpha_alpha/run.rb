61460b2fc65209b66dc1248a411d6203c6e1eaf5
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-14 18:59:04 -0600

    commit 4 to pool[rapid-steaming-cow-sh1wrc] at 18:59 - 14/01/2018

--- alpha_alpha/run.rb
+++ alpha_alpha/run.rb
@@ -1,3 +1,11 @@
 #metaprogramming - self-writing code
 require 'ostruct'
 
+class Author
+    attr_accessor :fname, :lname, :genre
+
+    def author
+        OpenStruct.new(fname: fname, lname: lname, genre: genre)
+    end
+
+end
