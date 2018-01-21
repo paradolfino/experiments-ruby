a793aa4b822aab1b75ca7c5cb122d9b80d63f422
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 18:45:55 -0600

    commit 18 to pool[rapid-steaming-rabbit-rvunq4] at 18:45 - 20/01/2018

--- alpha_gamma/run2.rb
+++ alpha_gamma/run2.rb
@@ -1,7 +1,7 @@
 #api - built in httparty methods
 require 'rubygems'
 require 'httparty'
-
+ENV['SSL_CERT_FILE'] = 'D:\RailsInstaller\cacert.pem'
 response = HTTParty.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
 puts response.body
