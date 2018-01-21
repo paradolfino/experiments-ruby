f5b3c150f903c04bd1726384e83a8931611678ae
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 18:15:36 -0600

    commit 13 to pool[rapid-steaming-rabbit-rvunq4] at 18:15 - 20/01/2018

--- alpha_gamma/run2.rb
+++ alpha_gamma/run2.rb
@@ -2,16 +2,4 @@
 require 'rubygems'
 require 'httparty'
 
-# class EdutechionalResty
-#     include HTTParty
-#     base_uri "edutechional-resty.herokuapp.com"
-
-#     def posts
-#         self.class.get('/posts.json')
-#     end
-# end
-
-# edu = EdutechionalResty.new
-# puts edu.posts
-
 response = HTTParty.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow')
