0dae45dfae9c1ddbc48dd2937eaf4d81894c85a8
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 18:13:22 -0600

    commit 10 to pool[rapid-steaming-rabbit-rvunq4] at 18:13 - 20/01/2018

---
+++ alpha_gamma/run2.rb
@@ -0,0 +1,15 @@
+#api
+require 'rubygems'
+require 'httparty'
+
+# class EdutechionalResty
+#     include HTTParty
+#     base_uri "edutechional-resty.herokuapp.com"
+
+#     def posts
+#         self.class.get('/posts.json')
+#     end
+# end
+
+# edu = EdutechionalResty.new
+# puts edu.posts
