216114c79e9f9d0d5318893e7ca8d7ab52fd9e65
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 18:19:19 -0600

    commit 14 to pool[rapid-steaming-rabbit-rvunq4] at 18:19 - 20/01/2018

--- alpha_gamma/run2.rb
+++ alpha_gamma/run2.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'httparty'
 
-response = HTTParty.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow')
+require 'https'
 
-puts response.body
+http = Net::HTTP.new('example.com', 443)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+http.cert_store = OpenSSL::X509::Store.new
+http.cert_store.set_default_paths
+http.cert_store.add_file('/path/to/cacert.pem')
+
+# response = HTTParty.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow')
+
+# puts response.body
